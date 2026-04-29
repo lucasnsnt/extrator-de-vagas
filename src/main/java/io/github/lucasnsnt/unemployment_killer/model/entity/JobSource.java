0843b22a23b8fa8f99bfcebcf62edbb9b5a1a924
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2026-04-29 17:46:26 -0300

    feat: add JPA entities / initial project structure

---
+++ src/main/java/io/github/lucasnsnt/unemployment_killer/model/entity/JobSource.java
@@ -0,0 +1,64 @@
+package io.github.lucasnsnt.unemployment_killer.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(
+        name = "job_source",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"job_id", "source", "url"})
+        }
+)
+public class JobSource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String source;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String url;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "job_id", nullable = false)
+    private Job job;
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/io/github/lucasnsnt/unemployment_killer/scraper/core.java
@@ -0,0 +1,4 @@
+package io.github.lucasnsnt.unemployment_killer.scraper;
+
+public class core {
+}
