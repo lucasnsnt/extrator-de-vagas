78ff0524f03146e4d2a5c5b6a678fad98e9199a3
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2026-04-30 17:09:24 -0300

    feat: implementado jpa entities

--- src/main/java/io/github/lucasnsnt/unemployment_killer/model/entity/Job.java
+++ src/main/java/io/github/lucasnsnt/unemployment_killer/model/entity/Job.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,27 +14,104 @@ import java.util.List;
 public class Job {
 
     @Id
+    @Column(updatable = false)
     private String id;
 
     @NotBlank
     @Column(nullable = false)
     private String title;
 
-    @NotNull
-    @Column(nullable = false)
     private String company;
-
 
     private String location;
 
     @Column(length = 5000)
     private String description;
 
-    private String publishedAt;
+    private LocalDate publishedAt;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    private String status;
 
     @OneToMany(
             mappedBy = "job",
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private List<JobSource> sources = new ArrayList<>();
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(LocalDate publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public List<JobSource> getSources() {
+        return sources;
+    }
+
+    public void setSources(List<JobSource> sources) {
+        this.sources = sources;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
