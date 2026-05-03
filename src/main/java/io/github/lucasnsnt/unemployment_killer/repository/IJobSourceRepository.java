f5a377714ad0e5267a2487f250931f8b1f037e6c
Lucas Rocha Santos <lucas.santos04@souunit.com.br>
2026-05-03 20:03:40 -0300

    feat: add job processing and hash generation

--- src/main/java/io/github/lucasnsnt/unemployment_killer/repository/IJobSourceRepository.java
+++ src/main/java/io/github/lucasnsnt/unemployment_killer/repository/IJobSourceRepository.java
@@ -1,9 +1,14 @@
 package io.github.lucasnsnt.unemployment_killer.repository;
 
+import io.github.lucasnsnt.unemployment_killer.model.entity.Job;
 import io.github.lucasnsnt.unemployment_killer.model.entity.JobSource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IJobSourceRepository extends JpaRepository<JobSource, String> {
+
+    Optional<JobSource> findByJobAndSourceAndUrl(Job job, String source, String url);
 }
